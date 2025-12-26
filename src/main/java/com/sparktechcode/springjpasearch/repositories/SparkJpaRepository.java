6750a47569f88e93098035bf911e38314ed40f81
Akif Hadziabdic <akifh@spark-tech.io>
2025-12-26 19:20:54 +0100

    Improve logging

--- src/main/java/com/sparktechcode/springjpasearch/repositories/SparkJpaRepository.java
+++ src/main/java/com/sparktechcode/springjpasearch/repositories/SparkJpaRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
-public interface SparkJpaRepository<I, E extends BaseEntity<I>> extends JpaRepository<E, String>, JpaSpecificationExecutor<E> {
+public interface SparkJpaRepository<I, E extends BaseEntity<I>> extends JpaRepository<E, I>, JpaSpecificationExecutor<E> {
 }
